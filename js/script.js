8c8d92b73037db6c9e1c93cf9a3bd07ed28f13b0
JulianJulesHernandez <108310215+JulianJulesHernandez@users.noreply.github.com>
2022-07-04 20:24:54 -0700

    Step 1 complete

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// STEP 1 Convert the following highlighted identifiers to Camel Case notation
+
+// let some_month;
+// function the Month();
+// let current-month;
+// let summer_month;
+// let Mylibrary-function;
+
+// let someMonth;
+// function theMonth();
+// let currentMonth;
+// let summerMonth;
+// let myLibraryFunction;
